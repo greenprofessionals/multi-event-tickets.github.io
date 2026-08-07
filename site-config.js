4fb6eea68d006e55d47951c75fde5e05b8a29516
greenprofessionals <green.professionals.network@gmail.com>
2026-08-07 00:22:18 -0400

    Update site-config.js

--- site-config.js
+++ site-config.js
@@ -2,6 +2,6 @@
 // 1) Deploy the NEW Code.gs as a Web App and paste its /exec URL below.
 // 2) Run createEventConfigForm() and optionally paste the published form URL.
 window.MULTI_EVENT_CONFIG = {
-  ENDPOINT: 'PASTE_NEW_APPS_SCRIPT_WEB_APP_URL_HERE',
-  FORM_URL: 'PASTE_NEW_GOOGLE_FORM_PUBLISHED_URL_HERE'
+  ENDPOINT: 'https://script.google.com/macros/s/AKfycbzfJUvM3sA4fJ0tL467N_crI2pq_uB2K3UK9N3hTynFxVT7LoloB75y2k0bOE33aTf1pQ/exec',
+  FORM_URL: 'https://docs.google.com/forms/d/e/1FAIpQLSdbQ1Lrms5o05k7h-iaxNfeCvygQ94y5FwEgXsx2MhVtmxp9Q/viewform'
 };
